bd7bd0dc1963afa204d9ce8e9ba70542e3f8687c
Muhammad Hamza <hamza3256@live.co.uk>
2025-04-29 18:01:33 +0100

    chore: prevent scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import { Background } from "@/components/background";
-
+import { cn } from "@/lib/utils";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className, "overflow-hidden")}>
         <Background />
         <Providers>{children}</Providers>
       </body>
