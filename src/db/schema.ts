74ffc6bdd371650bc2741695bb30a572afb41fee
Muhammad Hamza <hamza3256@live.co.uk>
2025-05-06 18:37:25 +0100

    feat: add accounts to schema; make password nullable; account linking allowed

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,13 +4,14 @@ import {
   timestamp,
   uuid,
   primaryKey,
+  integer,
 } from "drizzle-orm/pg-core";
 
 export const users = pgTable("user", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name"),
   email: text("email").notNull().unique(),
-  password: text("password").notNull(),
+  password: text("password"),
   username: text("username").unique(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
@@ -37,3 +38,26 @@ export const verificationTokens = pgTable(
     compoundKey: primaryKey(vt.identifier, vt.token),
   }),
 );
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: uuid("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<"oauth" | "email" | "oidc">().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey(account.provider, account.providerAccountId),
+  })
+);
+ 
