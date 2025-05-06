2773e9a632ff23be49e446a592b3537fa983024d
Muhammad Hamza <hamza3256@live.co.uk>
2025-05-06 22:39:31 +0500

    feat: add accounts to schema, make password nullable, account linking (#6)
    * feat: add accounts to schema; make password nullable; account linking allowed
    
    * fix: format

---
+++ drizzle/0001_sparkling_skin.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "account" (
+	"user_id" uuid NOT NULL,
+	"type" text NOT NULL,
+	"provider" text NOT NULL,
+	"provider_account_id" text NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" text,
+	"scope" text,
+	"id_token" text,
+	"session_state" text,
+	CONSTRAINT "account_provider_provider_account_id_pk" PRIMARY KEY("provider","provider_account_id")
+);
+--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -66,6 +66,7 @@ if (
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
   );
 }
