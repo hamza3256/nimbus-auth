63dde4e4b8c9cfd9ba7d39d605d9d13582bc76fa
Muhammad Hamza <hamza3256@live.co.uk>
2025-05-09 00:56:58 +0500

    feat: update video, improve responsiveness for mobile (#8)

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -8,10 +8,13 @@ export function Background() {
         loop
         muted
         playsInline // Essential for iOS autoplay
-        className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto object-cover transform -translate-x-1/2 -translate-y-1/2"
+        className="absolute top-1/2 left-[25%] md:left-1/2 min-w-[150%] md:min-w-full min-h-full w-auto h-auto object-cover transform -translate-x-1/2 -translate-y-1/2"
         poster="https://res.cloudinary.com/dn20h4mis/image/upload/e_enhance/q_auto/f_auto/background_zhum8q" // Fallback image
       >
-        <source src="/background.mp4" type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/dn20h4mis/video/upload/v1746728104/background_rzsthe.mp4"
+          type="video/mp4"
+        />
         Your browser does not support the video tag.
       </video>
       {/* 
